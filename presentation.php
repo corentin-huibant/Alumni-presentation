c175b4e5e5600a6a49c0c4cd8e904c8273e3be86
Corentin <corentin.huibant@laposte.net>
2017-02-02 15:55:20 +0100

    nouveau merge

--- presentation.php
+++ presentation.php
@@ -1,30 +1,31 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
-    <title>presentation</title>
-  </head>
-  <body>
-      <div id="presentation_contents">
-        <table width="0%" border="0" align="center">
-          <tr>
-            <td><h1>Mission et services</h1>
-            <h2>ALUMNI E2N, le réseau des nouveaux développeur de sites web
-              et d’applications mobiles, intégrateur, référent numérique,
-              datartisan, e-commerçant…</h2>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <title>presentation</title>
+</head>
+<body>
+  <div id="presentation_contents">
+    <table width="0%" border="0" align="center">
+      <tr>
+        <td><h1>Mission et services</h1>
+          <h2>ALUMNI E2N, le réseau des nouveaux développeur de sites web
+            et d’applications mobiles, intégrateur, référent numérique,
+            datartisan, e-commerçant…</h2>
             <p>l’E2N (pour École du Numérique du Noyonnais) est un projet initié par la
-               communauté de communes du pays Noyonnais, qui est aujourd’hui porté par
-               Novei formation et simplon,co,
-                    </br>
-               E2N est une formation qui forme aux métiers du numérique et du développement Web,
-               basée sur le Campus Inovia à Noyon. Entièrement gratuite pour ses apprenant(e)s, la formation est soutenue et immersive (35 heures par semaine) sur une durée de six mois.
-                    </br>
-               Après deux semaines de test intensives, les élèves décident si ils veulent
-               poursuive ou non la formation une fois leur décision prise  ils ont six mois
-               pour atteindre leur but : faire de la création et l’animation de sites Internet.</p></td>
-            </tr>
-          </table>
-        </div>
+              communauté de communes du pays Noyonnais, qui est aujourd’hui porté par
+              Novei formation et simplon,co.</p>
+          
+            <p>E2N est une formation qui forme aux métiers du numérique et du développement Web,
+            basée sur le Campus Inovia à Noyon. Entièrement gratuite pour ses apprenant(e)s,
+            la formation est soutenue et immersive (35 heures par semaine) sur une durée de six mois.</p>
+
+          <p>Après deux semaines de test intensives, les élèves décident si ils veulent
+          poursuive ou non la formation une fois leur décision prise  ils ont six mois
+          pour atteindre leur but : faire de la création et l’animation de sites Internet.</p></td>
+        </tr>
+      </table>
+    </div>
   </body>
   </html>
