3e7c9da76722bca1103f3e5d51b6c57cf75f3bca
Virginie <leguilcher.virgie@outlook.fr>
2017-02-02 14:14:05 +0100

    presentation

--- presentation.php
+++ presentation.php
@@ -6,6 +6,15 @@
     <title>presentation</title>
   </head>
   <body>
-    <p></p>
+    <p>l’E2N (pour École du Numérique du Noyonnais) est un projet initié par la
+       communauté de communes du pays Noyonnais, qui est aujourd’hui porté par
+       Novei formation et simplon,co,
+
+E2N est une formation qui forme aux métiers du numérique et du développement Web,
+ basée sur le Campus Inovia à Noyon. Entièrement gratuite pour ses apprenant(e)s, la formation est soutenue et immersive (35 heures par semaine) sur une durée de six mois.
+
+Après deux semaines de test intensives, les élèves décident si ils veulent
+ poursuive ou non la formation une fois leur décision prise  ils ont six mois
+ pour atteindre leur but : faire de la création et l’animation de sites Internet. </p>
   </body>
 </html>
