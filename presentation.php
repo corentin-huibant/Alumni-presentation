1db0959c0c21e83c97ea21fdd5ce409dca9753d7
nabil-leroy <nabileroy@yahoo.fr>
2017-02-02 13:30:34 +0100

    Rectangle pour introduction

--- presentation.php
+++ presentation.php
@@ -1,11 +1,33 @@
+
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
-    <title>presentation</title>
-  </head>
-  <body>
-    <p></p>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <title></title>
+</head>
+<body>
+  <div class="presentation_contents">
+    <table width="0%" border="0" align="center">
+      <tr>
+        <td><p align="center">TEXTE</td>
+        </tr>
+      </table>
+    </div>
+    <div class="presentation_contents">
+      <table width="0%" border="0" align="center">
+        <tr>
+          <td><p align="center">TEXTE</td>
+          </tr>
+        </table>
+      </div>
+      <div class="presentation_contents">
+        <table width="0%" border="0" align="center">
+          <tr>
+            <td><p align="center">TEXTE</td>
+            </tr>
+          </table>
+        </div>
   </body>
-</html>
+  </html>
