fa3c33ad4245e826ad3fee2d03429a895a8d0a21
Corentin <corentin.huibant@laposte.net>
2017-02-02 13:41:23 +0100

    premier texte

--- presentation.php
+++ presentation.php
@@ -6,6 +6,23 @@
     <title>presentation</title>
   </head>
   <body>
-    <p></p>
+    <h1>Mission et services</h1>
+    <h2>ALUMNI E2N, le réseau des nouveaux développeur de sites web
+      et d’applications mobiles, intégrateur, référent numérique,
+      datartisan, e-commerçant…</h2>
+    <p>Entreprise de l’économie sociale et solidaire (agrément ESUS),
+      Simplon.co est un réseau de « fabriques » (écoles) qui propose
+      des formations GRATUITES pour devenir développeur de sites web
+      et d’applications mobiles, intégrateur, référent numérique,
+      datartisan, e-commerçant… et bien d’autres métiers numériques
+      “en tension” qui permettent de trouver rapidement un emploi ou
+      de créer sa propre activité. Ces formations, qualifiantes ou certifiantes,
+      s’adressent prioritairement aux personnes éloignées de l’emploi
+      (jeunes de moins de 25 ans, peu ou pas diplômées, issues des quartiers populaires
+      et des milieux ruraux, demandeurs d’emplois de longue durée, seniors, personnes
+      en situation de handicap, personnes réfugiées), et sont ouvertes sur critères
+      sociaux et avec un objectif de parité hommes-femmes mais sans aucun pré-requis
+      technique (débutants acceptés). En revanche, il est obligatoire d’avoir une très forte
+      MOTIVATION, une appétence forte pour le numérique et d’aimer travailler en équipe !</p>
   </body>
 </html>
