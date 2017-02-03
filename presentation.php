6d431c3ede46de6a5f490ddb930b1491d3410235
Corentin <corentin.huibant@laposte.net>
2017-02-03 12:09:21 +0100

    finalisation

--- presentation.php
+++ presentation.php
@@ -6,6 +6,35 @@
   <title>presentation</title>
 </head>
 <body>
+  <fieldset id="date">
+     <SCRIPT LANGUAGE="JavaScript1.2">
+     var mois=new Array(13);
+     mois[1]="Janvier";
+     mois[2]="Février";
+     mois[3]="Mars";
+     mois[4]="Avril";
+     mois[5]="Mai";
+     mois[6]="Juin";
+     mois[7]="Juillet";
+     mois[8]="Août";
+     mois[9]="Septembre";
+     mois[10]="Octobre";
+     mois[11]="Novembre";
+     mois[12]="Décembre";
+     var time=new Date();
+     var month=mois[time.getMonth() + 1];
+     var date=time.getDate();
+     var year=time.getYear();
+     if (year < 2000)
+     year = year + 1900;
+     document.write("Aujourd'hui " +date + " ");
+     document.write(month + " " + year);
+     </SCRIPT>
+   </fieldset>
+   <div id = "presentation_bloc1">
+   <table width="0%" border="0" align="center">
+     </table>
+   </div>
   <div id="presentation_contents">
     <table width="0%" border="0" align="center">
       <tr>
@@ -16,7 +45,7 @@
             <p>l’E2N (pour École du Numérique du Noyonnais) est un projet initié par la
               communauté de communes du pays Noyonnais, qui est aujourd’hui porté par
               Novei formation et simplon,co.</p>
-          
+
             <p>E2N est une formation qui forme aux métiers du numérique et du développement Web,
             basée sur le Campus Inovia à Noyon. Entièrement gratuite pour ses apprenant(e)s,
             la formation est soutenue et immersive (35 heures par semaine) sur une durée de six mois.</p>
